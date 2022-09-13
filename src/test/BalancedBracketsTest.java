19f744f7251f7ac80e4b4806bb50e08c809bab54
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-09-12 19:50:09 -0500

    Worked through studio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -3,14 +3,61 @@ package test;
 import main.BalancedBrackets;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
+//assert True
+    String s1 = "[]";
+    String s2 = "brackets dont exist";
+    String s3 = "[brackets do exist]";
+    String s4 = "[brackets] do [exist]";
+    String s5 = "][";
+//assert false
+    String s6 ="[[silly goose these are not brackets]";
+
+    String s7 = "[only 1 bracket does exist";
+
+    String s8 =  "only 1 bracket does exist]";
+
+    String s9 = "{silly goose these are not brackets}";
+
+    String s10 = "(silly goose these are not brackets)";
+
+    String s11 = "[]silly goose there are too many closing brackets]";
+
+    String s12 = "[[silly goose there are too many open brackets]";
+
 
     @Test
+    public void trueTestValid() {
+        ArrayList<String> Tests = new ArrayList<>(Arrays.asList(s1, s2, s3, s4, s5));
+        for (String TrueTest : Tests) {
+            assertTrue(BalancedBrackets.hasBalancedBrackets(TrueTest));
+
+        }
+
+
+    }
+    @Test
+    public void falseTestValid() {
+        ArrayList<String> Tests = new ArrayList<>(Arrays.asList(s6, s7, s8, s9, s10, s11, s12));
+        for (String FalseTest : Tests) {
+            assertFalse(BalancedBrackets.hasBalancedBrackets(FalseTest));
+
+        }
+    }
+}
+
+/*
+@Test
     public void onlyBracketsReturnsTrue() {
         assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
+
+
     @Test
     public void bracketsExist() {
         assertTrue(BalancedBrackets.hasBalancedBrackets("brackets dont exist"));
@@ -32,13 +79,19 @@ public class BalancedBracketsTest {
         assertFalse(BalancedBrackets.hasBalancedBrackets("only 1 bracket does exist]"));
     }
     @Test
-    public void reversedBrackets() {
-        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    public void onlyBracketsReversed() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("]["));
     }
     @Test
-    public void wrongBrackets() {
-        assertFalse(BalancedBrackets.hasBalancedBrackets("{silly goose these are not brackets}"));
+    public void wrongOpenBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[silly goose these are not brackets]" ));
     }
+    @Test
+    public void wrongCloseBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]silly goose these are not brackets]" ));
+    }
+
+
 
     //TODO: add tests here
     @Test
@@ -47,4 +100,4 @@ public class BalancedBracketsTest {
     }
 
 
-}
+}*/
